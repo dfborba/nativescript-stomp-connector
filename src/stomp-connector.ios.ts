a11b24be34e6f1b3f362ccf9570cfed200fc7f55
dborba <daniel@witfy.io>
2020-10-02 06:05:39 -0300

    ios start version

--- src/stomp-connector.ios.ts
+++ src/stomp-connector.ios.ts
@@ -1,28 +1,32 @@
-import { StompConfig, StompHeaders, StompMessage } from './stomp-connector.common';
+import { StompConfig, StompMessage } from './stomp-connector.common';
 
-class StompClientLibDelegateImpl extends NSObject implements StompClientLibDelegate {
+export var StompClientLibDelegate: {
+	prototype: StompClientLibDelegate;
+};
+
+class MyStompClientLibDelegateImpl extends NSObject implements StompClientLibDelegate {
 	public static ObjCProtocols = [StompClientLibDelegate];
 	private _owner: WeakRef<any>;
 
-	public static initWithOwner(owner: WeakRef<any>): StompClientLibDelegateImpl {
+	public static initWithOwner(owner: WeakRef<any>): MyStompClientLibDelegateImpl {
         let delegate = this.new();
         delegate._owner = owner;
         return delegate;
 	}
 	
-	static new(): StompClientLibDelegateImpl {
-        return <StompClientLibDelegateImpl>super.new();
+	static new(): MyStompClientLibDelegateImpl {
+        return <MyStompClientLibDelegateImpl>super.new();
 	}
 
 	stompClientDidReceiveMessageWithJSONBody?(client: StompClientLib, jsonBody: string): void {
 		console.log(`stompClientDidReceiveMessageWithJSONBody: ${jsonBody}`);
 	}
 
-	stompClientDidReceiveMessageWithJSONBodyWithHeader?(client: StompClientLib, jsonBody: string, headers: [String: string]): void {
+	stompClientDidReceiveMessageWithJSONBodyWithHeader?(client: StompClientLib, jsonBody: string, headers: any): void {
 		console.log(`stompClientDidReceiveMessageWithJSONBodyWithHeader: ${jsonBody} | ${headers}`);
 	}
 
-	stompClientDidReceiveMessageWithJSONBodyWithHeaderWithDestination?(client: StompClientLib, jsonBody: string, headers: [String: string], destination: string): void {
+	stompClientDidReceiveMessageWithJSONBodyWithHeaderWithDestination?(client: StompClientLib, jsonBody: string, headers: any, destination: string): void {
 		console.log(`stompClientWithDidReceiveMessageWithJSONBodyWithHeaderWithDestination: ${jsonBody} | ${headers} | ${destination}`);
 	}
 
@@ -60,12 +64,12 @@ export class StompConnector {
 		topics: [{ destination: string, callback: (payload: StompMessage) => {} }?], 
         messages: [{ destination: string, callback: (payload: StompMessage) => {} }?]};
 	
-	private _iosDelegate: StompClientLibDelegateImpl;
+	private _iosDelegate: MyStompClientLibDelegateImpl;
     private _mStompClient: StompClientLib;
 
 	constructor() {
 		this._callbacks = { topics: [], messages: [] };
-		this._iosDelegate = StompClientLibDelegateImpl.initWithOwner(this);
+		this._iosDelegate = MyStompClientLibDelegateImpl.initWithOwner(new WeakRef(this));
 	}
 
     public connect(config: StompConfig): void {
@@ -75,9 +79,14 @@ export class StompConnector {
 
 		this._mStompClient = StompClientLib.new();
 		if (!!config.connectHeaders) {
-			this._mStompClient.openSocketWithURLRequest
+			this._mStompClient.openSocketWithURLRequest(
+				NSURLRequest.requestWithURL(NSURL.URLWithString(config.brokerURL)),
+				this._iosDelegate,
+				config.connectHeaders);
 		} else {
-
+			this._mStompClient.openSocketWithURLRequest(
+				NSURLRequest.requestWithURL(NSURL.URLWithString(config.brokerURL)),
+				this._iosDelegate);
 		}
 	}
 
@@ -89,7 +98,7 @@ export class StompConnector {
 		this._mStompClient.subscribe(destination);
 	}
 
-    public send(message: string, toDestination: string, withHeaders?: [String: string], withReceipt?: string): void {
+    public send(message: string, toDestination: string, withHeaders?: any, withReceipt?: string): void {
 		if (!!withHeaders) {
 			if (!!withReceipt) {
 				this._mStompClient.sendMessageToDestinationWithHeadersWithReceipt(message, toDestination, withHeaders, withReceipt);
